7b754fd2ba4a84decf6edde42f25f46b03e56b1b
nathscrapps <jc4782071@gmail.com>
2022-08-20 23:36:16 -0500

    Creación de componentes de busqueda

---
+++ src/components/FilterByName.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="search">
+    <input 
+      type="text" 
+      name="search" 
+      placeholder="Search by name" 
+      v-model="name"
+      @keyup="filter()"
+    >
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useStore } from 'vuex';
+
+export default {
+  setup() {
+    const store = useStore();
+    const name = ref();
+
+    const filter = () =>{
+      store.dispatch('filterByName', name.value);
+    }
+    return {
+      name,
+      filter
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+    .search {
+        max-width: 400px;
+        margin: 3rem auto 0;
+        input {
+            height: 53px;
+            width: 400px;
+            border: none;
+            border-radius: 10px;
+            padding: 0 0.5rem;
+        }
+        &:active{
+            border: none;
+        }
+    }
+</style>
